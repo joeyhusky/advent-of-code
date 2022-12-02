164f819f452b4af4cad9fa32c42fbcf7ff2501f0
joeyhusky <76567879+joeyhusky@users.noreply.github.com>
2022-12-02 15:43:14 -0800

    2021 day2

--- 2021/day2/a.ts
+++ 2021/day2/a.ts
@@ -1,8 +1,26 @@
-const text = await Deno.readTextFile("./example.txt");
+const text = await Deno.readTextFile("./input.txt");
 
 const instructions = text.split("\n");
 
+let depth = 0;
+let horizontal = 0;
+let aim = 0;
+
+type Dir = "forward" | "down" | "up";
+
 for (const instruction of instructions) {
-  const [direction, n] = instruction.split(" ");
-  console.log(`direction ${direction}, amount ${n}`);
+  const [direction, n] = instruction.split(" ") as [Dir, string];
+  const amount = Number(n);
+  console.log(direction, n);
+  if (direction === "down") {
+    aim += amount;
+  } else if (direction === "up") {
+    aim -= amount;
+  } else {
+    horizontal += amount;
+    depth += aim * amount;
+  }
 }
+
+console.log(`dpeth: ${depth}, horizontal: ${horizontal}`);
+console.log(`multiplied: ${depth * horizontal}`);
