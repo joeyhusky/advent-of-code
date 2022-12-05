66abca9802f88da7494c57b9139be25ae2cb3fac
joeyhusky <76567879+joeyhusky@users.noreply.github.com>
2022-12-04 21:43:20 -0800

    add couple helpers

--- 2022/day4/a.ts
+++ 2022/day4/a.ts
@@ -1,22 +1,23 @@
-const text = await Deno.readTextFile("./input.txt");
+import { withInputData } from "../init.ts";
 
-const pairs = text.split('\n');
+withInputData((text) => {
+  const pairs = text.split("\n");
+  const main = () => {
+    let numOverlap = 0;
+    for (const pair of pairs) {
+      const [A, B] = pair.split(",");
+      const rangeA = A.split("-").map(Number);
+      const rangeB = B.split("-").map(Number);
+      if (fullyContains(rangeA, rangeB)) numOverlap++;
+      console.log("first ", rangeA, "\nsecond ", rangeB);
+    }
+    console.log("num overlapping ", numOverlap);
+  };
 
-const main = () => {
-  let numOverlap = 0;
-  for (const pair of pairs) {
-    const [A, B] = pair.split(',');
-    const rangeA = A.split('-').map(Number);
-    const rangeB = B.split('-').map(Number);
-    if (fullyContains(rangeA, rangeB)) numOverlap++;
-    console.log('first ', rangeA, '\nsecond ', rangeB);
-  }
-  console.log('num overlapping ', numOverlap);
-}
-
-const fullyContains = (rangeA: number[], rangeB: number[]): boolean => {
-  if (rangeA[0] >= rangeB[0] && rangeA[1] <= rangeB[1]) return true;
-  if (rangeB[0] >= rangeA[0] && rangeB[1] <= rangeA[1]) return true;
-  return false;
-}
-main();
+  const fullyContains = (rangeA: number[], rangeB: number[]): boolean => {
+    if (rangeA[0] >= rangeB[0] && rangeA[1] <= rangeB[1]) return true;
+    if (rangeB[0] >= rangeA[0] && rangeB[1] <= rangeA[1]) return true;
+    return false;
+  };
+  main();
+});
