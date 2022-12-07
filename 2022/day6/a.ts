3375b4c76b9a28b1035b98bab711ab8dd567a1d9
joeyhusky <76567879+joeyhusky@users.noreply.github.com>
2022-12-06 21:20:20 -0800

    day 6

--- 2022/day6/a.ts
+++ 2022/day6/a.ts
@@ -1,7 +1,7 @@
 import { getInputData } from "../../utils.ts";
 
 const line = await getInputData();
-console.log(line);
+
 class Buffer {
   private data: string;
   private array: (string | null)[];
@@ -14,21 +14,18 @@ class Buffer {
     this.data = data;
   }
 
-  processCharacter(): number | null {
-    this.array.unshift(line.charAt(this.buffIdx));
-    if (this.array.length >= this.buffIdx) {
-      if (this.isUnique()) {
-        return this.buffIdx;
-      }
+  processCharacter(idx: number): number | null {
+    this.array.unshift(line.charAt(idx));
+    if (this.array.length === this.size && this.isUnique()) {
+      return this.buffIdx;
     }
     if (this.array.length >= this.size) this.array.pop();
     return null;
   }
 
   processData(): number {
-    let foundIdx = this.processCharacter();
-    while (this.buffIdx++ < this.data.length) {
-      foundIdx = this.processCharacter();
+    while (this.buffIdx < this.data.length) {
+      const foundIdx = this.processCharacter(this.buffIdx++);
       if (foundIdx) {
         console.log("found marker", foundIdx);
         return foundIdx;
@@ -38,10 +35,9 @@ class Buffer {
   }
 
   private isUnique(): boolean {
-    console.log("checking array:", this.array);
     return new Set(this.array).size === this.size;
   }
 }
 
-const b = new Buffer(line, 4);
+const b = new Buffer(line, 14);
 b.processData();
